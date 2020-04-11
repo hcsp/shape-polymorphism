100ff3d0b7593b0cb63dcb09273a6ecfeb29c22f
strivejojo <1603427597@qq.com>
2020-04-11 22:28:48 +0800

    Java面向对象初步练习：创建多态的父类和子类

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -11,6 +11,6 @@ public class Circle extends Shape {
 
     @Override
     public double getArea() {
-        return Math.PI * diameter * diameter;
+        return Math.PI * diameter/2 * diameter/2;
     }
 }
