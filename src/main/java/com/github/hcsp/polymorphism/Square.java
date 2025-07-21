ff7da52cc0f1100a0a104f9f6a8f3688af72cd24
lyra <lyraaaaaaaaaaa@gmail.com>
2025-07-21 10:10:56 +1000

    polymorphism

--- src/main/java/com/github/hcsp/polymorphism/Square.java
+++ src/main/java/com/github/hcsp/polymorphism/Square.java
@@ -1,10 +1,17 @@
 package com.github.hcsp.polymorphism;
 
-public class Square {
+public class Square extends Shape{
     // 正方形的边长
     private double sideLength;
 
     public Square(double sideLength) {
         this.sideLength = sideLength;
     }
+
+    @Override
+    public double getArea() {
+        double result = sideLength*sideLength;
+        return result;
+    }
+
 }
